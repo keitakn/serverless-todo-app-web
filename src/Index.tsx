fa8cb1515e602822429e819cf512521d738b0a37
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-04-25 01:05:31 +0900

    add HelloWorldが表示出来る事を確認する為に追加

---
+++ src/Index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import MyComponent from "./components/MyComponent";
+
+ReactDOM.render(<MyComponent content="Hello React SPA!!"/>, document.getElementById("app"));
