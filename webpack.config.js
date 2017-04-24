9dfb3111142652bf91e953a6e386d24408ab9d51
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-04-25 01:04:55 +0900

    add コンパイルに必要な設定ファイル郡を追加

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/Index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: 'ts-loader'}
+        ]
+      }
+    ]
+  }
+};
