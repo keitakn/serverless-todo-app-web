e5719e7a9e11ea8d1c49c9ff106fa8e60a0d24a5
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-04-29 23:57:54 +0900

    modify #7 MuiThemeProviderの呼出をComponent側に移動

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,14 +1,11 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import * as injectTapEventPlugin from "react-tap-event-plugin";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import MyComponent from "./components/MyComponent";
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <MuiThemeProvider>
-    <MyComponent content="Hello React SPA!" />
-  </MuiThemeProvider>,
+  <MyComponent content="Hello React SPA!" />,
   document.getElementById("app")
 );
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import RaisedButton from "material-ui/RaisedButton";
 
 export interface Props {
   content: string;
-};
+}
 
 const style = {
   margin: 12,
@@ -12,12 +13,14 @@ const style = {
 export default class MyComponent extends React.Component<Props, {}> {
   render() {
     return (
-      <div>
-        <RaisedButton label="Default" style={style} />
-        <RaisedButton label={this.props.content} primary={true} style={style} />
-        <RaisedButton label="Secondary" secondary={true} style={style} />
-        <RaisedButton label="Disabled" disabled={true} style={style} />
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <RaisedButton label="Default" style={style} />
+          <RaisedButton label={this.props.content} primary={true} style={style} />
+          <RaisedButton label="Secondary" secondary={true} style={style} />
+          <RaisedButton label="Disabled" disabled={true} style={style} />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
