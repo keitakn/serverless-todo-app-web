106780f431ffc29d409bc80d9d1a0f72e64b335e
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-05-31 00:35:33 +0900

    add #16 server側のtodo登録処理を実装

--- server.ts
+++ server.ts
@@ -1,16 +1,53 @@
 import * as express from "express";
 import * as path from "path";
+import * as fs from "fs";
+import * as bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const TODO_LIST_JSON = path.join(__dirname, 'todo.json');
 
 app.use("/dist", express.static("dist"));
+app.use(bodyParser.json());
 
 app.get("/api/count", (req, res) => {
   res.contentType("application/json");
   const obj = {amount: 100};
   setTimeout(() => res.json(obj), 500);
   //res.status(400).json(obj); //for error testing
+});
+
+app.post("/api/todo", (req, res) => {
+
+  fs.readFile(TODO_LIST_JSON, (error, buffer) => {
+    res.contentType("application/json");
+
+    const todo = JSON.parse(buffer.toString());
+
+    if (error) {
+      res.status(500).json(
+        {"message": "500 Internal Server Error"}
+      );
+      return;
+    }
+
+    const addTodo = {
+      id: Date.now(),
+      title: req.body.title
+    };
+
+    todo.push(addTodo);
+    fs.writeFile(TODO_LIST_JSON, JSON.stringify(todo, null, 2), (error) => {
+      if (error) {
+        res.status(500).json(
+          {"message": "500 Internal Server Error"}
+        );
+      }
+
+      setTimeout(() => res.status(201).json(todo), 500);
+      return;
+    });
+  });
 });
 
 app.get("*", (req, res) => {
--- src/todo/Container.tsx
+++ src/todo/Container.tsx
@@ -4,14 +4,28 @@ import {Dispatch} from "redux";
 import {ReduxAction, ReduxState} from "../store";
 import {addAction} from "./module";
 import Todo from "./Todo";
+import {AxiosInstance} from "axios";
+import HttpClientFactory from "../factories/HttpClientFactory";
 
 export class ActionDispatcher {
-  constructor(private dispatch: (action: ReduxAction) => void) {}
+  constructor(private dispatch: (action: ReduxAction) => void, private axiosInstance: AxiosInstance) {}
 
-  public addTodo(title: string): void {
-    this.dispatch(addAction(title));
+  public async addTodo(title: string): Promise<void> {
+    try {
+      const axiosResponse = await this.axiosInstance.post("/api/todo", {title: title});
+
+      if (axiosResponse.status !== 201) {
+        throw new Error(`illegal status code: ${axiosResponse.status}`);
+      }
+
+      this.dispatch(addAction(title));
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
+
+const axiosInstance = HttpClientFactory.create();
 
 const mapStateToProps = (state: ReduxState, ownProps: RouteComponentProps<{myParams: string | undefined}>) => {
   if (ownProps.match.params.myParams === undefined) {
@@ -21,7 +35,7 @@ const mapStateToProps = (state: ReduxState, ownProps: RouteComponentProps<{myPar
 };
 
 const mapDispatchToProps = (dispatch: Dispatch<ReduxAction>) => (
-    {actions: new ActionDispatcher(dispatch)}
+    {actions: new ActionDispatcher(dispatch, axiosInstance)}
   );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Todo);
