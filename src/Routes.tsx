313d369b9e301c0e80a1188d4ef298e0585933db
keita-nishimoto <k.mail.09070876640@icloud.com>
2017-09-18 03:02:23 +0900

    modify #32 Menu、Routing用のComponentを関数型Componentに変更

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,15 +5,20 @@ import NotFound from './components/NotFound';
 import Signup from './pages/signup/Container';
 import Todo from './pages/todo/Container';
 
-export default class Routes extends React.Component<{}, {}> {
+/**
+ * ルーティング用 Component
+ *
+ * @returns {any}
+ * @constructor
+ */
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact={true} path="/todo" component={Todo} />
+      <Route exact={true} path="/signup" component={Signup} />
+      <Route component={NotFound}/>
+    </Switch>
+  );
+};
 
-  public render() {
-    return (
-      <Switch>
-        <Route exact={true} path="/todo" component={Todo} />
-        <Route exact={true} path="/signup" component={Signup} />
-        <Route component={NotFound}/>
-      </Switch>
-    );
-  }
-}
+export default Routes;
--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -3,31 +3,36 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class AppMenu extends React.Component<any, any> {
+/**
+ * Menu用Component
+ *
+ * @returns {any}
+ * @constructor
+ */
+const AppMenu = () => {
+  return (
+    <AppBar
+      title="TODOアプリ"
+      iconElementLeft={
+        <IconMenu
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+        >
+          <MenuItem
+            primaryText="signup"
+            containerElement={
+              <Link to="/signup" />
+            }
+          />
+          <MenuItem
+            primaryText="todo"
+            containerElement={
+              <Link to="/todo" />
+            }
+          />
+        </IconMenu>
+      }
+    />
+  );
+};
 
-  public render() {
-    return (
-      <AppBar
-        title="TODOアプリ"
-        iconElementLeft={
-          <IconMenu
-            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-          >
-            <MenuItem
-              primaryText="signup"
-              containerElement={
-                <Link to="/signup" />
-              }
-            />
-            <MenuItem
-              primaryText="todo"
-              containerElement={
-                <Link to="/todo" />
-              }
-            />
-          </IconMenu>
-        }
-      />
-    );
-  }
-}
+export default AppMenu;
